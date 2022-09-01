19802848b36c96495189de14615e5351548fd64f
Vladislav Udovenko <vladislav.udovenko0@yandex.ru>
2022-09-02 00:44:32 +0500

    Added the EmployeeService.java interface

---
+++ src/main/java/org/springapp/service/EmployeeService.java
@@ -0,0 +1,15 @@
+package org.springapp.service;
+
+import org.springapp.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    public List<Employee> getAllEmployees();
+
+    public void saveEmployee(Employee employee);
+
+    public Employee getEmployee(int id);
+
+    public void deleteEmployee(int id);
+}
