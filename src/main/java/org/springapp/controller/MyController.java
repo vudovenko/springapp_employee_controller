811c62f187868a7aa046bbcde4b3b3d59c1297ed
Vladislav Udovenko <vladislav.udovenko0@yandex.ru>
2022-09-02 01:11:22 +0500

    Added query matching in the MyController.java

--- src/main/java/org/springapp/controller/MyController.java
+++ src/main/java/org/springapp/controller/MyController.java
@@ -1,13 +1,49 @@
 package org.springapp.controller;
 
+import org.springapp.entity.Employee;
 import org.springapp.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MyController {
     @Autowired
     private EmployeeService employeeService;
 
+    @RequestMapping("/")
+    public String showAllEmployees(Model model) {
+        var allEmployees = employeeService.getAllEmployees();
+        model.addAttribute("allEmps", allEmployees);
+        return "all-employees";
+    }
 
+    @RequestMapping("/addNewEmployee")
+    public String addNewEmployee(Model model) {
+        var employee = new Employee();
+        model.addAttribute("employee", employee);
+        return "employee-info";
+    }
+
+    @RequestMapping("/saveEmployee")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+        employeeService.saveEmployee(employee);
+        return "redirect:/";
+    }
+
+    @RequestMapping("/updateInfo")
+    public String updateEmployee(@RequestParam("empId") int id, Model model) {
+        var employee = employeeService.getEmployee(id);
+        model.addAttribute("employee", employee);
+        return "employee-info";
+    }
+
+    @RequestMapping("/deleteEmployee")
+    public String deleteEmployee(@RequestParam("empId") int id) {
+        employeeService.deleteEmployee(id);
+        return "redirect:/";
+    }
 }
