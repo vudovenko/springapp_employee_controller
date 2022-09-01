6d98ca4b9ba633c6874a3d9798ee275f5f11756e
Vladislav Udovenko <vladislav.udovenko0@yandex.ru>
2022-09-02 00:41:21 +0500

    Added @Repository annotation over EmployeeDAOImpl.java

--- src/main/java/org/springapp/dao/EmployeeDAOImpl.java
+++ src/main/java/org/springapp/dao/EmployeeDAOImpl.java
@@ -4,10 +4,12 @@ import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 import org.springapp.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
 import java.util.List;
 
+@Repository
 public class EmployeeDAOImpl implements EmployeeDAO {
     @Autowired
     private SessionFactory sessionFactory;
