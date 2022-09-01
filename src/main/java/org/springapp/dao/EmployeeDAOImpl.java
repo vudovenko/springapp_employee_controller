3110516619e6ee895128996f825ef73aa2f6f0cc
Vladislav Udovenko <vladislav.udovenko0@yandex.ru>
2022-09-02 01:13:07 +0500

    Clearing Imports in EmployeeDAOImpl.java

--- src/main/java/org/springapp/dao/EmployeeDAOImpl.java
+++ src/main/java/org/springapp/dao/EmployeeDAOImpl.java
@@ -6,7 +6,6 @@ import org.springapp.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
