e8f5d4e127508eb4fcf0e5196c0e1d86508c8c67
Vladislav Udovenko <vladislav.udovenko0@yandex.ru>
2022-09-02 01:46:20 +0500

    Adding an aspect of the MyLoggingAspect.java class

---
+++ src/main/java/org/springapp/aspect/MyLoggingAspect.java
@@ -0,0 +1,27 @@
+package org.springapp.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class MyLoggingAspect {
+    @Around("execution(* org.springapp.dao.*.*(..))")
+    public Object aroundAllRepositoryMethodsAdvice
+            (ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        MethodSignature methodSignature = (MethodSignature) proceedingJoinPoint.getSignature();
+
+        String methodName = methodSignature.getName();
+        System.out.println("********************************************");
+        System.out.println("Begin of " + methodName);
+
+        Object targetMethodResult = proceedingJoinPoint.proceed();
+
+        System.out.println("End of " + methodName);
+        System.out.println("********************************************");
+        return targetMethodResult;
+    }
+}
