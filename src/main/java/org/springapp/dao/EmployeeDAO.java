490ac0f3f5afd3bf465e3f2fe927ffad5634be2f
Vladislav Udovenko <vladislav.udovenko0@yandex.ru>
2022-09-02 00:08:38 +0500

    Add EmployeeDAO.java interface

---
+++ src/main/java/org/springapp/dao/EmployeeDAO.java
@@ -0,0 +1,15 @@
+package org.springapp.dao;
+
+import org.springapp.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+    public List<Employee> getAllEmployees();
+
+    public void saveEmployee(Employee employee);
+
+    public Employee getEmployee(int id);
+
+    public void deleteEmployee(int id);
+}
