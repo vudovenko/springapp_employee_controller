6adb4e4dc7f963f7d679c53a400a26e0df77ffa6
Vladislav Udovenko <vladislav.udovenko0@yandex.ru>
2022-09-02 00:28:47 +0500

    Implemented the interface in the class EmployeeDAOImpl.java

--- src/main/java/org/springapp/dao/EmployeeDAOImpl.java
+++ src/main/java/org/springapp/dao/EmployeeDAOImpl.java
@@ -1,27 +1,44 @@
 package org.springapp.dao;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springapp.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 public class EmployeeDAOImpl implements EmployeeDAO {
+    @Autowired
+    private SessionFactory sessionFactory;
+
     @Override
     public List<Employee> getAllEmployees() {
-        return null;
+        var session = sessionFactory.getCurrentSession();
+        var employees = session
+                .createQuery("from Employee", Employee.class)
+                .getResultList();
+        return employees;
     }
 
     @Override
     public void saveEmployee(Employee employee) {
-
+        var session = sessionFactory.getCurrentSession();
+        session.saveOrUpdate(employee);
     }
 
     @Override
     public Employee getEmployee(int id) {
-        return null;
+        var session = sessionFactory.getCurrentSession();
+        return session.get(Employee.class, id);
     }
 
     @Override
     public void deleteEmployee(int id) {
-
+        var session = sessionFactory.getCurrentSession();
+        Query<Employee> query = session.createQuery("delete from Employee " +
+                "where id =:employeeId");
+        query.setParameter("employeeId", id);
+        query.executeUpdate();
     }
 }
