f61e57a2aa8d13a1cac04645b6cbbab0925bdb58
Vladislav Udovenko <vladislav.udovenko0@yandex.ru>
2022-09-02 00:11:31 +0500

    Add EmployeeDAOImpl.java class

---
+++ src/main/java/org/springapp/dao/EmployeeDAOImpl.java
@@ -0,0 +1,27 @@
+package org.springapp.dao;
+
+import org.springapp.entity.Employee;
+
+import java.util.List;
+
+public class EmployeeDAOImpl implements EmployeeDAO {
+    @Override
+    public List<Employee> getAllEmployees() {
+        return null;
+    }
+
+    @Override
+    public void saveEmployee(Employee employee) {
+
+    }
+
+    @Override
+    public Employee getEmployee(int id) {
+        return null;
+    }
+
+    @Override
+    public void deleteEmployee(int id) {
+
+    }
+}
